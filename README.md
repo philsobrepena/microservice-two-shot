ff64f6a6284b10f6bd9d2d5c1c88f62edf0417ee
Kat3lyn5 <kati3lyn@gmail.com>
2023-07-19 12:29:17 -0700

    added hats_project in settings and updated readme with microservice partner info

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Team:
 
-* Person 1 - Which microservice?
-* Person 2 - Which microservice?
+* Katelyn Chang -Hats
+* Phil Sobrepena - Shoes
 
 ## Design
 
--- hats/api/hats_project/settings.py
+++ hats/api/hats_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "hats_rest.apps.AccountsConfig",
     "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
