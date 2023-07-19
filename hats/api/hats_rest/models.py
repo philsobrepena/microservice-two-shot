7e664ab0c4b95840392e507f4c0179400832eb3d
Kat3lyn5 <kati3lyn@gmail.com>
2023-07-19 16:02:57 -0700

    created hat_rest models.py

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class LocationVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Hat(models.Models):
+    fabric = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    color = models.CharField(max_length=255)
+    picture = models.URLField(max_length=200, null=True)
