844e6137d88891d45a72fcdae2b24f10a5feeb71
phil sobrepena <JPLS791@gmail.com>
2023-07-19 16:08:01 -0700

    hats_rest hat model

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class BinVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
+
+class Shoe(models.Model):
+    manufacturer = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    picture_url = models.URLField(null=True)
+    bin = models.ForeignKey(
+        BinVO,
+        related_name="shoes",
+        on_delete=models.CASCADE
+    )
