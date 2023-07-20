91052d11ef91cb759ab928b2652128b0fc2f4c29
phil sobrepena <JPLS791@gmail.com>
2023-07-19 18:12:12 -0700

    shoes_rest views + api_urls, shoes_project urls

--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -1,3 +1,80 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+from common.json import ModelEncoder
+from .models import BinVO, Shoe
 
 # Create your views here.
+
+class BinVODetailEncoder(ModelEncoder):
+    model = BinVO
+    properties = ["name", "import_href"]
+
+class ShoeListEncoder(ModelEncoder):
+    model = Shoe
+    properties = ["name"]
+
+
+    # def get_extra_data(self, o):
+    #     return {"conference": o.conference.name}
+
+class ShoeDetailEncoder(ModelEncoder):
+    model = Shoe
+    properties = [
+        "manufacturer",
+        "name",
+        "color",
+        "picture_url",
+        "bin",
+    ]
+    encoders = {
+        "bin": BinVODetailEncoder(),
+    }
+
+    # def get_extra_data(self, o):
+    #     count = BinVO.objects.filter(name=o.name).count()
+    #     return {"has_name": count > 0}
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_shoes(request, bin_vo_id=None):
+
+    if request.method == "GET":
+        if bin_vo_id is not None:
+            shoes = Shoe.objects.filter(bin=bin_vo_id)
+        else:
+            shoes = Shoe.objects.all()
+        return JsonResponse(
+            {"shoes": shoes},
+            encoder=ShoeListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+
+        try:
+            bin_href = content["bin"]
+            bin = BinVO.objects.get(import_href=bin_href)
+            content["bin"] = bin
+        except BinVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid bin id"},
+                status=400,
+            )
+
+        shoe = Shoe.objects.create(**content)
+        return JsonResponse(
+            shoe,
+            encoder=ShoeDetailEncoder,
+            safe=False,
+        )
+
+
+def api_show_shoe(request, pk):
+
+    shoe = Shoe.objects.get(id=pk)
+    return JsonResponse(
+        shoe,
+        encoder=ShoeDetailEncoder,
+        safe=False,
+    )
