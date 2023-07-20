b01d270532a75b44132ce534485b60d8cd46200e
Kat3lyn5 <kati3lyn@gmail.com>
2023-07-19 19:21:27 -0700

    completed poller.py code structure/pseudocode

--- hats/poll/poller.py
+++ hats/poll/poller.py
@@ -9,15 +9,25 @@ sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hats_project.settings")
 django.setup()
 
+from hats.api.hats_rest.models import LocationVO
 # Import models from hats_rest, here.
 # from hats_rest.models import Something
+
+def get_locations():
+    response = requests.get("http://http://wardrobe-api:8000/api/locations/")
+    content = json.loads(response.content)
+    for location in content["locations"]:
+        LocationVO.objects.update_or_create(
+            import_href=location["href"],
+            defaults={"name": location["name"]},
+        )
+
 
 def poll():
     while True:
         print('Hats poller polling for data')
         try:
-            # Write your polling logic, here
-            pass
+            get_locations()
         except Exception as e:
             print(e, file=sys.stderr)
         time.sleep(60)
