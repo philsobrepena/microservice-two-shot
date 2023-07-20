2447f98dce3281741524b36e523c51c8c9e4bd08
Kat3lyn5 <kati3lyn@gmail.com>
2023-07-19 18:12:28 -0700

    created my list_hats and show_hats as well as encoders.

--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
+from .models import Hat, LocationVO
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+from common.json import ModelEncoder
 
 # Create your views here.
+
+class LocationVODetailEncoder(ModelEncoder):
+    model = LocationVO
+    properties = ["name", "import_href"]
+
+class HatListEncoder(ModelEncoder):
+    model = Hat
+    properties = [
+        "fabric",
+        "name",
+        "color",
+        "picture",
+
+    ]
+
+class HatDetailEncoder(ModelEncoder):
+    model = Hat
+    properties = [
+        "fabric"
+        "name"
+        "color"
+        "picture"
+    ]
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_hats(request):
+    if request.method == "GET":
+        hat = Hat.objects.all()
+        return JsonResponse(
+            {"hat": hat},
+            encoder = HatListEncoder)
+    else:
+        content = json.loads(request.body)
+
+        try:
+            location_href = content["location"]
+            location = LocationVO.objects.get(import_href=location_href)
+            content["location"] = location
+        except LocationVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid conference id"},
+                status=400,
+            )
+
+        hat = Hat.objects.create(**content)
+        return JsonResponse(
+            hat,
+            encoder=HatDetailEncoder,
+            safe=False,
+        )
+
+def api_show_hats(request, pk):
+
+    hat = Hat.objects.get(id=pk)
+    return JsonResponse(
+        hat,
+        encoder=HatDetailEncoder,
+        safe=False,
+    )
